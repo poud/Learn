3d107e1800803df4584d26a24c2eb9d87e75dfae
Fredrik Ekholdt <freekh@gmail.com>
2016-04-21 22:30:08 +0200

    comments (more bewildering)

--- snake.js
+++ snake.js
@@ -32,13 +32,13 @@ var state = [
   [ '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '']
 ];
 
-//Usually it is better to wait until html is loaded before calling JavaScript 'stuff'.
-//We know it is loaded when the DOMContentLoaded EVENT is triggered
+//_Usually it is better to wait until html is loaded before calling JavaScript 'stuff'.
+//_We know it is loaded when the DOMContentLoaded EVENT is triggered
 document.addEventListener('DOMContentLoaded', function() {
   render(state, rootElem);
 })
 
-//Remember these functions are hoisted (google it ;) ) - I will ask what it means ;)
+//_Remember these functions are hoisted (google it ;) ) - I will ask what it means ;)
 function render(state, rootElem) {
   var rows = state.length;
   var currentTableElem = tableElem();
@@ -46,7 +46,7 @@ function render(state, rootElem) {
     var columns = state[row].length;
     currentTableElem.appendChild(rowElem()); //just add another row, don't need anything else
     for (column = 0; column < columns; column++) {
-      currentTableElem.appendChild(cellElem(state[row][column]));
+      currentTableElem.appendChild(cellElem(state[row][column])); //add the actual cell
     }
   }
   clear(rootElem);
@@ -67,7 +67,7 @@ function rowElem() {
 
 function cellElem(cell) {
   var cellElem = document.createElement('td');
-  if (cell === 'S') {//Send me a mail when you see this and tell me what the difference between === and == is (or ask if you don't remember)
+  if (cell === 'S') { //_Send me a mail when you see this and tell me what the difference between === and == is (or ask if you don't remember)
     cellElem.setAttribute('class', 'Board-Cell Board-Snake-Body');
   } else if (cell === 'H') {
     cellElem.setAttribute('class', 'Board-Cell Board-Snake-Head');
